7e4fb0dba8340c4e1ff2a28abc8c29b8b615106a
Buseong Kim <flight@skyline23.com>
2026-04-04 23:08:12 +0900

    Revert "Reduce HEVC frame delay"
    This reverts commit f16fb2399a9c469cbbd5c3d7ea8cb8b1f6956c0f.

--- Sources/MacDisplayCaptureKit/CaptureVideoToolboxProcessing.swift
+++ Sources/MacDisplayCaptureKit/CaptureVideoToolboxProcessing.swift
@@ -115,7 +115,7 @@ enum MDKVideoToolboxLatencyPolicy {
         case .h264:
             return 2
         case .hevc:
-            return 0
+            return 1
         case .proResProxy:
             return 0
         }
