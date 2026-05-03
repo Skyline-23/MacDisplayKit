79d9dc37cb95cc6eb608c6e358ba9ae57f5bad83
Buseong Kim <flight@skyline23.com>
2026-05-04 00:53:14 +0900

    Revert "experiment: read BGRA pixels in no-scale converter"
    This reverts commit 504388273daf0e586854e65ad2de1f8e394e6df0.

--- Sources/MacDisplayCaptureKit/CaptureMetalColorConversion.swift
+++ Sources/MacDisplayCaptureKit/CaptureMetalColorConversion.swift
@@ -82,8 +82,6 @@ final class MDKMetalBGRAToYCbCrConverter {
     private let device: any MTLDevice
     private let lumaPipeline: any MTLComputePipelineState
     private let chromaPipeline: any MTLComputePipelineState
-    private let noScaleReadLumaPipeline: any MTLComputePipelineState
-    private let noScaleReadChromaPipeline: any MTLComputePipelineState
     private let bgraCursorOverlayPipeline: any MTLComputePipelineState
     private let transparentCursorTexture: any MTLTexture
 
@@ -99,19 +97,15 @@ final class MDKMetalBGRAToYCbCrConverter {
 
         guard let lumaFunction = library.makeFunction(name: "bgraToYCbCrLuma"),
               let chromaFunction = library.makeFunction(name: "bgraToYCbCrChroma"),
-              let noScaleReadLumaFunction = library.makeFunction(name: "bgraToYCbCrLumaNoScaleRead"),
-              let noScaleReadChromaFunction = library.makeFunction(name: "bgraToYCbCrChromaNoScaleRead"),
               let bgraCursorOverlayFunction = library.makeFunction(name: "overlayCursorOnBGRA") else {
             throw MDKMetalColorConversionError.functionMissing(
-                "bgraToYCbCrLuma/bgraToYCbCrChroma/bgraToYCbCrLumaNoScaleRead/bgraToYCbCrChromaNoScaleRead/overlayCursorOnBGRA"
+                "bgraToYCbCrLuma/bgraToYCbCrChroma/overlayCursorOnBGRA"
             )
         }
 
         do {
             lumaPipeline = try device.makeComputePipelineState(function: lumaFunction)
             chromaPipeline = try device.makeComputePipelineState(function: chromaFunction)
-            noScaleReadLumaPipeline = try device.makeComputePipelineState(function: noScaleReadLumaFunction)
-            noScaleReadChromaPipeline = try device.makeComputePipelineState(function: noScaleReadChromaFunction)
             bgraCursorOverlayPipeline = try device.makeComputePipelineState(function: bgraCursorOverlayFunction)
         } catch {
             throw MDKMetalColorConversionError.pipelineCreationFailed(String(describing: error))
@@ -196,36 +190,24 @@ final class MDKMetalBGRAToYCbCrConverter {
             index: 0
         )
 
-        let usesNoScaleReadFastPath =
-            cursorOverlaySample == nil &&
-            target.chromaSubsampling == SIMD2<UInt32>(2, 2) &&
-            sourceTextures[0].width == destinationTextures[0].width &&
-            sourceTextures[0].height == destinationTextures[0].height
-        let lumaPipelineState = usesNoScaleReadFastPath ? noScaleReadLumaPipeline : lumaPipeline
-        let chromaPipelineState = usesNoScaleReadFastPath ? noScaleReadChromaPipeline : chromaPipeline
-
         computeEncoder.setTexture(sourceTextures[0], index: 0)
         computeEncoder.setTexture(destinationTextures[0], index: 1)
-        if !usesNoScaleReadFastPath {
-            computeEncoder.setTexture(cursorTexture ?? transparentCursorTexture, index: 2)
-        }
-        computeEncoder.setComputePipelineState(lumaPipelineState)
+        computeEncoder.setTexture(cursorTexture ?? transparentCursorTexture, index: 2)
+        computeEncoder.setComputePipelineState(lumaPipeline)
         dispatch(
             encoder: computeEncoder,
-            pipeline: lumaPipelineState,
+            pipeline: lumaPipeline,
             width: destinationTextures[0].width,
             height: destinationTextures[0].height
         )
 
         computeEncoder.setTexture(sourceTextures[0], index: 0)
         computeEncoder.setTexture(destinationTextures[1], index: 1)
-        if !usesNoScaleReadFastPath {
-            computeEncoder.setTexture(cursorTexture ?? transparentCursorTexture, index: 2)
-        }
-        computeEncoder.setComputePipelineState(chromaPipelineState)
+        computeEncoder.setTexture(cursorTexture ?? transparentCursorTexture, index: 2)
+        computeEncoder.setComputePipelineState(chromaPipeline)
         dispatch(
             encoder: computeEncoder,
-            pipeline: chromaPipelineState,
+            pipeline: chromaPipeline,
             width: destinationTextures[1].width,
             height: destinationTextures[1].height
         )
@@ -531,17 +513,6 @@ final class MDKMetalBGRAToYCbCrConverter {
         return sourceTexture.sample(linearSampler, normalizedCoordinates).rgb;
     }
 
-    inline float3 readRGB(
-        texture2d<float, access::read> sourceTexture,
-        uint2 pixel
-    ) {
-        uint2 clampedPixel = min(
-            pixel,
-            uint2(sourceTexture.get_width() - 1, sourceTexture.get_height() - 1)
-        );
-        return sourceTexture.read(clampedPixel).rgb;
-    }
-
     inline float3 transformRGB(
         float3 rgb,
         constant ConversionParameters &parameters
@@ -619,22 +590,6 @@ final class MDKMetalBGRAToYCbCrConverter {
         destinationTexture.write(limitedY, gid);
     }
 
-    kernel void bgraToYCbCrLumaNoScaleRead(
-        texture2d<float, access::read> sourceTexture [[texture(0)]],
-        texture2d<float, access::write> destinationTexture [[texture(1)]],
-        constant ConversionParameters &parameters [[buffer(0)]],
-        uint2 gid [[thread_position_in_grid]]
-    ) {
-        if (gid.x >= destinationTexture.get_width() || gid.y >= destinationTexture.get_height()) {
-            return;
-        }
-
-        float3 rgb = transformRGB(readRGB(sourceTexture, gid), parameters);
-        float y = dot(float4(rgb, 1.0), parameters.yCoefficients);
-        float limitedY = clamp((y * parameters.lumaScale) + parameters.lumaOffset, 0.0, 1.0);
-        destinationTexture.write(limitedY, gid);
-    }
-
     kernel void bgraToYCbCrChroma(
         texture2d<float, access::sample> sourceTexture [[texture(0)]],
         texture2d<float, access::write> destinationTexture [[texture(1)]],
@@ -671,34 +626,6 @@ final class MDKMetalBGRAToYCbCrConverter {
             }
         }
         rgb *= 1.0 / max(float(sampleCount), 1.0);
-
-        float cb = dot(float4(rgb, 1.0), parameters.cbCoefficients);
-        float cr = dot(float4(rgb, 1.0), parameters.crCoefficients);
-        float2 limitedUV = clamp(
-            (float2(cb, cr) * parameters.chromaScale) + parameters.chromaOffset,
-            float2(0.0),
-            float2(1.0)
-        );
-        destinationTexture.write(float4(limitedUV.x, limitedUV.y, 0.0, 1.0), gid);
-    }
-
-    kernel void bgraToYCbCrChromaNoScaleRead(
-        texture2d<float, access::read> sourceTexture [[texture(0)]],
-        texture2d<float, access::write> destinationTexture [[texture(1)]],
-        constant ConversionParameters &parameters [[buffer(0)]],
-        uint2 gid [[thread_position_in_grid]]
-    ) {
-        if (gid.x >= destinationTexture.get_width() || gid.y >= destinationTexture.get_height()) {
-            return;
-        }
-
-        uint2 basePixel = gid * parameters.chromaSubsampling;
-        float3 rgb =
-            transformRGB(readRGB(sourceTexture, basePixel), parameters) +
-            transformRGB(readRGB(sourceTexture, basePixel + uint2(1, 0)), parameters) +
-            transformRGB(readRGB(sourceTexture, basePixel + uint2(0, 1)), parameters) +
-            transformRGB(readRGB(sourceTexture, basePixel + uint2(1, 1)), parameters);
-        rgb *= 0.25;
 
         float cb = dot(float4(rgb, 1.0), parameters.cbCoefficients);
         float cr = dot(float4(rgb, 1.0), parameters.crCoefficients);
