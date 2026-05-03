89e27eca9bd7e168934215f77451d47bb60ed977
Buseong Kim <flight@skyline23.com>
2026-05-04 07:02:50 +0900

    Revert "experiment: preserve encoded source metadata"
    This reverts commit b577bedacce42dc4a9b0d7011d9576f9f9f08a58.

--- Sources/MacDisplayCaptureKit/CaptureVideoToolboxProcessing.swift
+++ Sources/MacDisplayCaptureKit/CaptureVideoToolboxProcessing.swift
@@ -352,8 +352,6 @@ public final class MDKVideoToolboxEncodingProcessor: MDKCaptureFrameProcessing, 
             pixelFormat: frame.pixelFormat,
             surface: surface,
             origin: frame.origin,
-            dirtyRects: frame.dirtyRects,
-            sourceUpdateDropCount: frame.sourceUpdateDropCount,
             cursorOverlaySample: frame.cursorOverlaySample,
             sourceCaptureDurationNanoseconds: frame.sourceCaptureDurationNanoseconds,
             sourceCursorCompositeDurationNanoseconds: frame.sourceCursorCompositeDurationNanoseconds
