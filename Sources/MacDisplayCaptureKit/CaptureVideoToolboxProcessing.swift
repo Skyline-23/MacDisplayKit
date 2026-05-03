bda313208ccb1f4aca1beb2e1079d0e797e47dfb
Buseong Kim <flight@skyline23.com>
2026-05-04 08:14:40 +0900

    Revert "experiment: deliver videotoolbox output immediately"
    This reverts commit 25454e9e5c291ffa8560c0e827d4fc4da6529a77.

--- Sources/MacDisplayCaptureKit/CaptureVideoToolboxProcessing.swift
+++ Sources/MacDisplayCaptureKit/CaptureVideoToolboxProcessing.swift
@@ -1649,10 +1649,6 @@ public final class MDKVideoToolboxEncodingProcessor: MDKCaptureFrameProcessing, 
             }
         }
         submissionToken?.markCompleted()
-        if let encodedFrame, outputCompleted {
-            outputHandler?(encodedFrame)
-        }
-
         outputQueue.async { [self] in
             outputCallbackCount += 1
             outputCallbackStatusHistogram[describe(status: status), default: 0] += 1
@@ -1672,7 +1668,9 @@ public final class MDKVideoToolboxEncodingProcessor: MDKCaptureFrameProcessing, 
             if outputCompleted {
                 completedOutputFrameCount += 1
             }
-            if status != noErr {
+            if let encodedFrame, outputCompleted {
+                outputHandler?(encodedFrame)
+            } else if status != noErr {
                 failureHandler?("VT output callback failed (\(describe(status: status))).")
             }
             outputDrainGroup.leave()
