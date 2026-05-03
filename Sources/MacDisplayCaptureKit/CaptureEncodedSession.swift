dd85cf1b233895cd680c9a3bb4b5869e2df3cb9d
Buseong Kim <flight@skyline23.com>
2026-05-04 06:23:03 +0900

    Revert "experiment: skip saturated idle replay mailbox"
    This reverts commit 9e5063c75036e6334116d8530c9140e95bb4a455.

--- Sources/MacDisplayCaptureKit/CaptureEncodedSession.swift
+++ Sources/MacDisplayCaptureKit/CaptureEncodedSession.swift
@@ -1139,9 +1139,6 @@ public actor MDKEncodedCaptureSession {
             sourceCadenceTracker?.record(displayTime: frame.displayTime)
             sourceTimingTracker?.record(frame: frame)
             guard pendingFrameTracker.tryAcquire(limit: maximumPendingFrameCount) else {
-                if frame.origin == .idleReplay {
-                    return
-                }
                 Task {
                     let replacedDisplayTime = await latestFrameMailbox.store(frame)
                     if let replacedDisplayTime {
